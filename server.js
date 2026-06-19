104f3f55d033f525d0ac22ce99a05f1209907f34
jose548gomes <jose548gomes@gmail.com>
2026-06-19 21:04:43 +0100

    Correções server.js, .env e .gitignore

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpecs = require('./swagger');
 const authRoutes = require('./routes/authRoutes');
-const recursoRoutes = require('./routes/[recurso]Routes'); // <-- MUDAR AQUI
+const subjectRoutes = require('./routes/subjectRoutes');
 const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
@@ -15,10 +15,10 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.use('/api/auth', authRoutes);
-app.use('/api/[recurso]', recursoRoutes); // <-- MUDAR AQUI
+app.use('/api/subjects', subjectRoutes);
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
-app.get('/', (req, res) => res.send('API [Recurso] - /api-docs'));
+app.get('/', (req, res) => res.send('API Disciplinas - /api-docs'));
 
 app.use(errorHandler);
 
@@ -27,4 +27,4 @@ mongoose.connect(process.env.MONGODB_URI)
     console.log('✅ Conectado ao MongoDB Atlas');
     app.listen(process.env.PORT, () => console.log(`🚀 Servidor na porta ${process.env.PORT}`));
   })
-  .catch(err => console.error(err));
+  .catch(err => console.error('❌ Erro ao conectar:', err));
